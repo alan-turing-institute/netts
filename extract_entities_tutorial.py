48d1d084226b0262ff9c0938355abec23c12e888
CN <cr.nettekoven@gmail.com>
2020-11-16 10:49:36 +0100

    Added extract entities tutorial script

--- extract_entities_tutorial.py
+++ extract_entities_tutorial.py
@@ -20,21 +20,10 @@ from stanza.server import CoreNLPClient
 import sys
 sys.path.append(
     '/Users/CN/Documents/Projects/Cambridge/cambridge_language_analysis/')
-# Create text pipeline (Load all of the default processors: tokenize, pos, lemma, depparse, sentiment & ner)
-nlp = stanza.Pipeline(lang='en')
+# Create text pipeline
+nlp = stanza.Pipeline(lang='en', processors='tokenize,ner')
 # Import text
-data_dir = '/Users/CN/Documents/Projects/Cambridge/data'
-f = open(op.join(data_dir, 'Kings',
-                 'Prolific_pilot_all_transcripts', '3138838-TAT13.txt'), 'r')
-text = f.read()
-f.close()
-
-doc = nlp(text)
-
-# Show PoS tags
-print(*[f'word: {word.text}\tupos: {word.upos}\txpos: {word.xpos}\tfeats: {word.feats if word.feats else "_"}' for sent in doc.sentences for word in sent.words], sep='\n')
-
-
+doc = nlp("Chris Manning teaches at Stanford University. He lives in the Bay Area.")
 # Extract entities from entire document
 print(*[f'entity: {ent.text}\ttype: {ent.type}' for ent in doc.ents], sep='\n')
 # Extract entities from sentence
