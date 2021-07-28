Fixed bug in averaging across tats and set random networks generator to 1000

cluster_graph_measures.py
@@ -47,33 +47,68 @@ graph_dir = '/Users/CN/Dropbox/speech_graphs/oasis'
 output_dir = op.join(graph_dir, 'output')
 output_figs = op.join(graph_dir, 'figures')
 
+
 # --------------------- Import all graph measures ---------------------------------------
 oasis = pd.read_csv(op.join(output_dir, 'graph_data_all_avg.csv'), index_col=0)
-corrMatrix = oasis.drop(columns='subj').corr()
+
+# Exclude subject 12 with bad quality transcript
+oasis = oasis[oasis.subj != 12]
+oasis.subj.unique()
+
+# --------------------- Exclude non-informative columns before correlating ---------------------------------------
+#
+# exclude_columns = ['average_total_degree_normF', 'parallel_edges_normF',
+#                    'parallel_edges_normZ', 'L1_normF', 'group_n', 'group', 'subj']
+#
+# --------------------- Select only columns that are in Sarahs paper and the connected component measures ---------------------------------------
+# selected_columns = ['words', 'sentences', 'mean_sentence_length', 'nodes', 'edges',
+#                     'cc_size_mean', 'cc_size_med', 'connected_components',
+#                     'max_degree_centrality', 'max_indegree_centrality_value', 'max_outdegree_centrality_value',
+#                     'cc_size_mean_normZ', 'cc_size_med_normZ', 'connected_components_normZ',
+#                     'syn_WC', 'syn_Nodes', 'syn_Edges', 'syn_RE', 'syn_PE', 'syn_L1',
+#                     'syn_L2', 'syn_L3', 'syn_LCC', 'syn_LSC', 'syn_ATD', 'syn_Density',
+#                     'syn_Diameter', 'syn_ASP', 'syn_CC', 'syn_Degree', 'syn_Degree.1',
+#                     'syn_Degree.2', 'syn_Degree.3', 'No. words', 'No. sent.',
+#                     'Sent. length', 'Coh.', 'Max similarity', 'Tangent', 'On-topic']
+
+# selected_columns = ['words', 'sentences', 'mean_sentence_length', 'nodes', 'edges',
+#                     'cc_size_mean', 'cc_size_med', 'connected_components',
+#                     'max_degree_centrality', 'max_indegree_centrality_value', 'max_outdegree_centrality_value',
+#                     'cc_size_mean_normZ', 'cc_size_med_normZ', 'connected_components_normZ',
+#                     'syn_LCC', 'syn_LSC', 'syn_CC',
+#                     'No. words', 'No. sent.', 'Sent. length',
+#                     'Coh.', 'Max similarity', 'Tangent', 'On-topic']
+
+selected_columns = ['nodes', 'edges', 'words', 'sentences', 'mean_sentence_length',
+                    'cc_size_mean', 'cc_size_med', 'connected_components',
+                    # 'max_degree_centrality', 'max_indegree_centrality_value', 'max_outdegree_centrality_value',
+                    # 'cc_size_mean_normZ', 'cc_size_med_normZ', 'connected_components_normZ',
+                    # 'syn_LCC', 'syn_LSC',
+                    'No. words', 'No. sent.', 'Sent. length',
+                    'Coh.', 'Max similarity', 'Tangent', 'On-topic']
 
 
-# --------------------- Plot correlation matrix ---------------------------------------
-
-# plt.figure(figsize=(25.6, 20))
-# # sns.heatmap(corrMatrix, mask=np.triu(corrMatrix), annot=True)
-# sns.heatmap(corrMatrix, annot=True)
-# output = op.join(
-#     output_figs, 'CorrMat_All_GraphProps')
-# plt.savefig(output)
-# plt.show()
+oasis_selected = oasis[selected_columns]
+corrMatrix = oasis_selected.corr()
 
 
 # --------------------- Louvain Clustering ---------------------------------------
 
 corr = corrMatrix.copy().to_numpy()
-nonegative[corr < 0] = 0
+nonegative = corr
+# nonegative[nonegative < 0] = 0
+nonegative = abs(nonegative)
 
-ci, Q = bct.community_louvain(nonegative, gamma=1)
+ci, Q = bct.community_louvain(nonegative)
 num_ci = len(np.unique(ci))
 print('{} clusters detected with a modularity of {:.2f}.'.format(num_ci, Q))
 
+ax = plotting.plot_mod_heatmap(corr, ci, vmin=-1, vmax=1, cmap='viridis',
+                               xticklabels=corrMatrix.columns, yticklabels=corrMatrix.columns, figsize=(13.5, 10), xlabelrotation=90)
+ax.set_xticklabels(corrMatrix.columns, rotation=30, ha="right")
 
-plotting.plot_mod_heatmap(corr, ci, vmin=-1, vmax=1, cmap='viridis',
-                          xticklabels=corrMatrix.columns, yticklabels=corrMatrix.columns)
-
+# plt.xticks(rotation=90)
+output = op.join(
+    output_figs, 'Clustered_oasis_slim_cc')
+plt.savefig(output)
 plt.show()

random_graphs.py
@@ -58,78 +58,83 @@ df.head()
 graph_sizes = [(len(list(G.nodes())), len(list(G.edges()))) for G in graphs]
 unique_graph_sizes = list(dict.fromkeys(graph_sizes))
 
-# # --------------------- Optional: Find appropriate number of random graphs where graph measures converge ---------------------------------------
-# # Choose largest and smallest by node
-# unique_graph_sizes.sort(key=lambda x: x[0])
-# graph_sizes_selection = [unique_graph_sizes[0], unique_graph_sizes[round(
-#     len(unique_graph_sizes) / 2)], unique_graph_sizes[-1]]
-#
-# # Choose largest and smallest by edge
-# unique_graph_sizes.sort(key=lambda x: x[1])
-# graph_sizes_selection.extend([unique_graph_sizes[0], unique_graph_sizes[round(
-#     len(unique_graph_sizes) / 2)], unique_graph_sizes[-1]])
-#
-#
-# # Create different number of random graphs for this sample of graph sizes
-# # steps = [10, 50]
-# steps = [10, 20, 40, 80, 100, 150, 200, 250, 400]
-# list_of_dataframes = []
-# for number_of_nodes, number_of_edges in graph_sizes_selection:
-#     for step in steps:
-#         random_graphs = []
-#         for i in range(0, step):
-#             random_graphs.append(nx.gnm_random_graph(
-#                 n=number_of_nodes, m=number_of_edges, directed=True))
-#         #
-#         # Collect properties of random graphs
-#         random_graph_properties = graph_properties_random(random_graphs)
-#         random_graph_properties['step'] = step
-#         list_of_dataframes.append(random_graph_properties)
-#
-# random_graph_data = pd.concat(list_of_dataframes)
-#
-# # --------------------- Plot where graph measures converge ---------------------------------------
-# # Plot convergence
+# --------------------- Optional: Find appropriate number of random graphs where graph measures converge ---------------------------------------
+# Choose largest and smallest by node
+unique_graph_sizes.sort(key=lambda x: x[0])
+graph_sizes_selection = [unique_graph_sizes[0], unique_graph_sizes[round(
+    len(unique_graph_sizes) / 2)], unique_graph_sizes[-1]]
+
+# Choose largest and smallest by edge
+unique_graph_sizes.sort(key=lambda x: x[1])
+graph_sizes_selection.extend([unique_graph_sizes[0], unique_graph_sizes[round(
+    len(unique_graph_sizes) / 2)], unique_graph_sizes[-1]])
+
+
+# Create different number of random graphs for this sample of graph sizes
+steps = [10, 50, 100, 200, 300, 400, 500, 600, 700,
+         800, 900, 1000, 1300, 1600, 2000, 2300, 2600, 3000]
+list_of_dataframes = []
+for number_of_nodes, number_of_edges in graph_sizes_selection:
+    for step in steps:
+        random_graphs = []
+        for i in range(0, step):
+            random_graphs.append(nx.gnm_random_graph(
+                n=number_of_nodes, m=number_of_edges, directed=True))
+        #
+        # Collect properties of random graphs
+        random_graph_properties = graph_properties_random(random_graphs)
+        random_graph_properties['step'] = step
+        list_of_dataframes.append(random_graph_properties)
+
+random_graph_data = pd.concat(list_of_dataframes)
+
+# --------------------- Plot where graph measures converge ---------------------------------------
+# Plot convergence
 # measures_of_interest = list(random_graph_data.columns)[2:-1]
-#
-# # for variable in measures_of_interest:
-# fig = plt.figure(figsize=(25, 10))
-# for v, variable in enumerate(measures_of_interest):
-#     #
-#     ax = plt.subplot(int(np.ceil(np.sqrt(len(measures_of_interest)))),
-#                      int(np.ceil(np.sqrt(len(measures_of_interest)))), v + 1)
-#     sns.lineplot(
-#         data=random_graph_data,
-#         x="step", y=variable, hue="nodes", style="nodes",
-#         markers=True, dashes=False, palette="husl"
-#     )
-#     plt.title(variable)
-#
-# output = op.join(output_figures, 'RandomGraphs_directed_Steps_nodeswise')
-# plt.savefig(output)
-# plt.show()
-#
-#
-# # for variable in measures_of_interest:
-# fig = plt.figure(figsize=(25, 10))
-# for v, variable in enumerate(measures_of_interest):
-#     #
-#     ax = plt.subplot(2, np.ceil(len(measures_of_interest) / 2), v + 1)
-#     sns.lineplot(
-#         data=random_graph_data,
-#         x="step", y=variable, hue="edges", style="edges",
-#         markers=True, dashes=False, palette="husl"
-#     )
-#     plt.title(variable)
-#
-# output = op.join(output_figures, 'RandomGraphs_directed_Steps_edgewise')
-# plt.savefig(output)
-# plt.show()
+# measures_of_interest = ['connected_components', 'cc_size_mean', 'cc_size_med',
+#                         'max_degree_centrality', 'max_indegree_centrality_value', 'max_outdegree_centrality_value']
+measures_of_interest = ['connected_components', 'cc_size_mean', 'cc_size_med']
+
+# for variable in measures_of_interest:
+fig = plt.figure(figsize=(25, 10))
+for v, variable in enumerate(measures_of_interest):
+    #
+    ax = plt.subplot(int(np.ceil(np.sqrt(len(measures_of_interest)))),
+                     int(np.ceil(np.sqrt(len(measures_of_interest)))), v + 1)
+    sns.lineplot(
+        data=random_graph_data,
+        x="step", y=variable, hue="nodes", style="nodes",
+        markers=True, dashes=False, palette="husl"
+    )
+    plt.title(variable)
+
+output = op.join(output_figures, 'RandomGraphs_directed_Steps_nodeswise_{}_3'.format(
+    str(datetime.date.today())))
+plt.savefig(output)
+plt.show()
+
+
+# for variable in measures_of_interest:
+fig = plt.figure(figsize=(25, 10))
+for v, variable in enumerate(measures_of_interest):
+    #
+    ax = plt.subplot(2, np.ceil(len(measures_of_interest) / 2), v + 1)
+    sns.lineplot(
+        data=random_graph_data,
+        x="step", y=variable, hue="edges", style="edges",
+        markers=True, dashes=False, palette="husl"
+    )
+    plt.title(variable)
+
+output = op.join(output_figures, 'RandomGraphs_directed_Steps_edgewise_{}_3'.format(
+    str(datetime.date.today())))
+plt.savefig(output)
+plt.show()
 
 
 # --------------------- Generate undirected random graphs for all graph sizes in dataset ---------------------------------------
 
-no_random_graphs = 300
+no_random_graphs = 1000
 list_of_dataframes = []
 for number_of_nodes, number_of_edges in unique_graph_sizes:
     random_graphs = []
@@ -143,6 +148,8 @@ for number_of_nodes, number_of_edges in unique_graph_sizes:
     # Add random graphs to dataframe
     random_graph_properties['graph'] = random_graphs
     list_of_dataframes.append(random_graph_properties)
+
+# XX CARO: CONTINUE HERE
 
 random_graph_data = pd.concat(list_of_dataframes)
 
@@ -176,9 +183,8 @@ for v, variable in enumerate(measures_of_interest):
 # --------------------- Write out normalised data ---------------------------------------
 # df.to_csv(op.join(output_dir, 'graph_data_normalised.csv'))
 
-# --------------------- Average across tats ---------------------------------------
 # Make subj and tat categorical
-df.subj = pd.Categorical(df.subj.astype('str'))
+df.subj = pd.Categorical(df.subj.astype('int'))
 df.tat = pd.Categorical(df.tat.astype('str'))
 
 
@@ -186,7 +192,7 @@ id_data = pd.read_csv(
     '/Users/CN/Documents/Projects/Cambridge/data/oasis/ids_oasis.csv', delimiter=';')
 df['group'] = np.nan
 for s, subj in enumerate(id_data.Subject):
-    df.at[df.subj == str(subj), 'group'] = id_data.Group[s]
+    df.at[df.subj.astype('str') == str(subj), 'group'] = id_data.Group[s]
 
 df.group = pd.Categorical(df.group.astype('str'))
 df.group = df.group.cat.rename_categories({'ARMS': 'CHR'})
@@ -198,21 +204,44 @@ print('--- Groups ---\n{}'.format(df.group.value_counts()))
 
 # Exclude subject 12 because of bad quality transcripts
 df.shape
-df = df[df.subj.values != '12']
-#  Average across TAT stimuli ( = one datapoint per participant )
+# df = df[df.subj.values != '12']
+
 df['group_n'] = None
 df.group_n = df.group.cat.codes * 100
-oasis_avg = (df.groupby((df.subj != df.subj.shift()).cumsum())
-             .mean()
-             .reset_index())
 
-oasis_avg['group'] = pd.Categorical(oasis_avg.group_n)
+# --------------------- Average across tats ---------------------------------------
+df_avg = (df.groupby((df.subj != df.subj.shift()).cumsum())
+          .mean()
+          .reset_index(drop=True))
 
-oasis_avg.group = oasis_avg.group.cat.rename_categories(
+# Add subj index that was dropped during averaging back into dataframe
+df_avg['subj'] = None
+for subject in df.subj.cat.categories:
+    # Find position where the average for words, sentences and nodes match the df_avg value
+    match_words = df.query('subj == @subject').mean().words == df_avg.words
+    match_sentences = df.query(
+        'subj == @subject').mean().sentences == df_avg.sentences
+    match_nodes = df.query(
+        'subj == @subject').mean().nodes == df_avg.nodes
+    intersection = np.logical_and(match_words, match_sentences)
+    intersection = np.logical_and(intersection, match_nodes)
+    df_avg.loc[intersection, 'subj'] = subject
+
+
+df_avg['group'] = pd.Categorical(df_avg.group_n)
+
+df_avg.group = df_avg.group.cat.rename_categories(
     {0: 'CON', -56: 'FEP', 100: 'CHR'})
-oasis_avg.group.cat.reorder_categories(
+df_avg.group.cat.reorder_categories(
     ['CON', 'CHR', 'FEP'], inplace=True)
-oasis_avg.group.value_counts()
+df_avg.group.value_counts()
+
+# (To control that subject assignment worked)
+# for subj in df.subj.cat.categories:
+#     print(df.query('subj==@subj').words.mean(), df_avg.query('subj==@subj').words)
+
+# --------------------- Write out normalised data averaged across tats ---------------------------------------
+df_avg.to_csv(op.join(output_dir, 'graph_data_normalised_avg.csv'))
 
 # --------------------- Compare groups ---------------------------------------
 
@@ -244,15 +273,15 @@ fig = plt.figure(figsize=(25, 14))
 for v, variable in enumerate(variable_list):
     ax = plt.subplot(2, np.ceil(len(variable_list) / 2), v + 1)
     sns.boxplot(y=variable, x='group',
-                data=oasis_avg,
+                data=df_avg,
                 palette="colorblind",
                 )
     results_mwu = []
     results_ttest = []
     stats_summary = ''
     for c, comb in enumerate(group_comparisons):
-        a = oasis_avg.query('group == @comb[0]')[variable]
-        b = oasis_avg.query('group == @comb[1]')[variable]
+        a = df_avg.query('group == @comb[0]')[variable]
+        b = df_avg.query('group == @comb[1]')[variable]
         results_mwu.append(scipy.stats.mannwhitneyu(a, b))
         results_ttest.append(scipy.stats.ttest_ind(a, b))
         # print('{0} vs. {1} {2} ({3})'.format(comb[0], comb[1], round(results_mwu[c][0],8), round(results_mwu[c][1],8) ))
@@ -274,25 +303,25 @@ output = op.join(output_figures, 'Hist_normalised_directed_2' +
 plt.savefig(output)
 plt.show()
 
-oasis_avg[oasis_avg.columns[58:]]
+df_avg[df_avg.columns[58:]]
 
 
 # ----------- Correlate
 
-scipy.stats.pearsonr(oasis_avg.cc_size_mean_normZ,
-                     oasis_avg.mean_sentence_length)
+scipy.stats.pearsonr(df_avg.cc_size_mean_normZ,
+                     df_avg.mean_sentence_length)
 
-scipy.stats.pearsonr(oasis_avg.cc_size_mean_normZ,
-                     oasis_avg.edges)
+scipy.stats.pearsonr(df_avg.cc_size_mean_normZ,
+                     df_avg.edges)
 
-scipy.stats.pearsonr(oasis_avg.cc_size_med_normZ,
-                     oasis_avg.mean_sentence_length)
+scipy.stats.pearsonr(df_avg.cc_size_med_normZ,
+                     df_avg.mean_sentence_length)
 
-scipy.stats.pearsonr(oasis_avg.cc_size_med_normZ,
-                     oasis_avg.mean_sentence_length)
+scipy.stats.pearsonr(df_avg.cc_size_med_normZ,
+                     df_avg.mean_sentence_length)
 
-scipy.stats.pearsonr(oasis_avg.cc_size_sd_normZ,
-                     oasis_avg.words)
+scipy.stats.pearsonr(df_avg.cc_size_sd_normZ,
+                     df_avg.words)
 
-scipy.stats.pearsonr(oasis_avg.cc_size_sd_normZ,
-                     oasis_avg.mean_sentence_length)
+scipy.stats.pearsonr(df_avg.cc_size_sd_normZ,
+                     df_avg.mean_sentence_length)