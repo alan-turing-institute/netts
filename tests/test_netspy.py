df423142b01f6862467256da496897dfe722d3f0
Helen Duncan <hduncan@turing.ac.uk>
2021-08-23 17:09:43 +0100

    Add speech graph function

--- tests/test_netspy.py
+++ tests/test_netspy.py
@@ -1,6 +1,13 @@
 # pylint: disable=C0114, C0116
 from netspy import __version__
-
+import netspy
 
 def test_version() -> None:
     assert __version__ == "0.1.0"
+
+def test_speech_graph():
+    text=open('demo_data/3138838-TAT10.txt','r')
+    transcript=text.read()
+    text.close()
+    graph=netspy.speech_graph(transcript)
+    print(type(graph))
