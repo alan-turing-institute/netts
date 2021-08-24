304cf533d3b06fa3d9162f71449ee5652e6dfa5a
Oscar Giles <ogiles@turing.ac.uk>
2021-08-24 15:28:09 +0100

    ✨ Add logging

--- netspy/install_models.py
+++ netspy/install_models.py
@@ -7,13 +7,20 @@ import nltk
 import stanza
 
 from netspy.config import get_settings
+from netspy.logger import logger
 
 
 def install_nltk_punk(netspy_dir: Optional[Union[str, Path]] = None) -> None:
 
     settings = get_settings(netspy_dir)
     settings.mk_netspy_dir()
-    nltk.download("punkt", download_dir=settings.nltk_dir)
+
+    logger.info("Downloading: NTLK punkt library to: %s", settings.nltk_dir)
+
+    if settings.nltk_dir.exists():
+        logger.warning("NLTK directory already exists: %s", settings.nltk_dir)
+        return
+    nltk.download("punkt", download_dir=settings.nltk_dir, quiet=True)
 
 
 def install_corenlp(netspy_dir: Optional[Union[str, Path]] = None) -> None:
@@ -21,6 +28,13 @@ def install_corenlp(netspy_dir: Optional[Union[str, Path]] = None) -> None:
     settings = get_settings(netspy_dir)
     settings.mk_netspy_dir()
 
+    logger.info("Downloading: Stanza CoreNLP library to: %s", settings.core_nlp_dir)
+
+    if settings.core_nlp_dir.exists():
+        logger.warning(
+            "Stanza CoreNLP directory already exists: %s", settings.core_nlp_dir
+        )
+        return
     stanza.install_corenlp(dir=settings.core_nlp_dir)
 
 
