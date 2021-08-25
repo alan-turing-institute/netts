4873c40df13cc8a7eeb293684166e6498b74effc
Oscar Giles <ogiles@turing.ac.uk>
2021-08-25 09:16:51 +0100

    🚧 Not working: downloading from google drive

--- netspy/install_models.py
+++ netspy/install_models.py
@@ -4,7 +4,11 @@ from pathlib import Path
 from typing import Optional, Union
 
 import nltk
+import requests
+from requests.sessions import session
 import stanza
+import tqdm
+from requests.models import ChunkedEncodingError, stream_decode_response_unicode
 
 from netspy.config import get_settings
 from netspy.logger import logger
@@ -38,9 +42,71 @@ def install_corenlp(netspy_dir: Optional[Union[str, Path]] = None) -> None:
     stanza.install_corenlp(dir=settings.core_nlp_dir)
 
 
+def install_openie5(netspy_dir) -> None:
+
+    def _get_confirmation_token(resp):
+
+        # Check for warning in cookie
+        for key, value in resp.cookies.items():
+            if key.startswith("download_warning"):
+                return value
+        return None
+        
+    settings = get_settings(netspy_dir)
+    settings.mk_netspy_dir()
+
+    url = "https://drive.google.com/uc?export=download"
+    params = {"id": "19z8LO-CYOfJfV5agm82PZ2JNWNUPIB6D"}
+
+    resp = requests.get(
+        url, params=params, stream=True
+    )
+    token = _get_confirmation_token(resp)
+
+
+    params.update({'confirm': token})
+    resp = requests.get(url, params, stream=True)
+    token = _get_confirmation_token(resp)
+
+    print(resp.headers)
+    print(resp.content)
+    return
+    # print( _get_confirmation_token(resp))
+    # # Check for confirmation token in cookie
+    # token = _get_confirmation_token(resp)
+    # if token:
+    #     params.update({'confirm': token})
+    #     resp = requests.get(url, params, stream=True)
+    # print( _get_confirmation_token(resp))
+
+    # print( resp.cookies.keys())
+
+    settings.openie5_dir.mkdir(exist_ok=True)
+    openie5_file = settings.openie5_dir / "openie-assembly-5.0-SNAPSHOT.jar"
+    openie5_file.touch()
+    
+    logger.info("Downloading: OpenIE5 to: %s", settings.openie5_dir)
+    with open(openie5_file, "wb") as f:
+        
+        file_size = int(resp.headers.get("content-length"))
+        chunk_size = 131072
+        desc = "Downloading " + url
+        with tqdm.tqdm(unit="B", unit_scale=True, desc=desc) as pbar:
+            for chunk in resp.iter_content(chunk_size=chunk_size):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+                    pbar.update(len(chunk))
+
+    resp.raise_for_status()
+
+    return resp.status_code
+
+
 def install_models(
     netspy_dir: Optional[Union[str, Path]] = None,
 ) -> None:
 
     install_nltk_punk(netspy_dir)
     install_corenlp(netspy_dir)
+    install_openie5(netspy_dir)
