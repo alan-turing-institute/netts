8b6ebfc7344cc314afb661f0d604dbac2332a80a
Helen Duncan <hduncan@turing.ac.uk>
2021-08-26 13:12:58 +0100

    unit test for eg 13 added

--- tests/test_netspy.py
+++ tests/test_netspy.py
@@ -13,9 +13,6 @@ def test_speech_graph() -> None:
     transcript = text.read()
     text.close()
     graph1 = speech_graph(transcript)
-    graph2 = speech_graph(
-        "There are two… There is a young girl and who and seems to be maid, sitting on a couch. The little girl seems to be upset, she’s looking away and she looks very dismissive. She has something on her hands which I’m not sure what it is exactly, seems like, like a baby doll or yes, something, something looks familiar, like a toy. And the maid seems to convince her of, of something, but her eyes are closed and her mouth is shut. Oh and it seems to… She seems to be holding the book, maybe so she’s reading, but and the girl is not interested at all"
-    )
     # assert graph1.__dict__ == graph2.__dict__
     expected_node_list = [
         "girl",
@@ -28,12 +25,41 @@ def test_speech_graph() -> None:
         "i",
         "eyes",
         "closed",
-        "interested",
+        "interested at all",
         "book",
         "something",
         "hands",
         "mouth",
         "young",
     ]
-    assert list(graph2.nodes()) == expected_node_list
     assert list(graph1.nodes()) == expected_node_list
+
+    text2 = open("demo_data/3138838-TAT13.txt", "r")
+    transcript2 = text2.read()
+    text2.close()
+    graph2 = speech_graph(transcript2)
+    expected_node_list2 = [
+        "it",
+        "midday",
+        "hats",
+        "heads",
+        "the men",
+        "presumably",
+        "tired",
+        "i",
+        "sort",
+        "picture",
+        "floor",
+        "one of the men",
+        "pain",
+        "he",
+        "asleep or very tired",
+        "man",
+        "this",
+        "one",
+        "men",
+        "face",
+        "person",
+        "all",
+    ]
+    assert list(graph2.nodes()) == expected_node_list2
