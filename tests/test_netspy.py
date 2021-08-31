e8b413ec53f370e4ec99458f3ff90e8cc944d7a9
Helen Duncan <hduncan@turing.ac.uk>
2021-08-31 15:25:30 +0100

    unit tests for all demo data passing locally

--- tests/test_netspy.py
+++ tests/test_netspy.py
@@ -152,3 +152,134 @@ def test_speech_graph() -> None:
     assert list(graph2.nodes()) == expected_node_list2
     assert list(graph2.edges()) == expected_edges2
     assert list(graph2.degree) == expected_degree2
+
+    text = open("demo_data/3138838-TAT30.txt", "r")
+    transcript = text.read()
+    text.close()
+    graph3 = speech_graph(transcript)
+    expected_node_list3 = [
+        "i",
+        "man",
+        "lightbulb",
+        "it",
+        "middle",
+        "there",
+        "picture",
+        "dark",
+        "anything",
+        "hoodie",
+        "jacket",
+        "hat",
+        "concept",
+        "very , very mysterious",
+        "standing",
+        "post",
+        "night",
+        "balls",
+        "reflections",
+        "context",
+        "light",
+    ]
+    expected_edges3 = [
+        ("i", "man"),
+        ("i", "lightbulb"),
+        ("i", "anything"),
+        ("i", "concept"),
+        ("i", "picture"),
+        ("man", "hoodie"),
+        ("man", "jacket"),
+        ("man", "hat"),
+        ("man", "picture"),
+        ("man", "standing"),
+        ("man", "post"),
+        ("it", "middle"),
+        ("it", "dark"),
+        ("middle", "night"),
+        ("there", "picture"),
+        ("picture", "very , very mysterious"),
+        ("standing", "dark"),
+        ("post", "light"),
+        ("balls", "reflections"),
+        ("context", "picture"),
+    ]
+    expected_degree3 = [
+        ("i", 5),
+        ("man", 7),
+        ("lightbulb", 1),
+        ("it", 2),
+        ("middle", 2),
+        ("there", 1),
+        ("picture", 5),
+        ("dark", 2),
+        ("anything", 1),
+        ("hoodie", 1),
+        ("jacket", 1),
+        ("hat", 1),
+        ("concept", 1),
+        ("very , very mysterious", 1),
+        ("standing", 2),
+        ("post", 2),
+        ("night", 1),
+        ("balls", 1),
+        ("reflections", 1),
+        ("context", 1),
+        ("light", 1),
+    ]
+
+    assert list(graph3.nodes()) == expected_node_list3
+    assert list(graph3.edges()) == expected_edges3
+    assert list(graph3.degree) == expected_degree3
+
+    text = open("demo_data/3138849-TAT10.txt", "r")
+    transcript = text.read()
+    text.close()
+    graph4 = speech_graph(transcript)
+
+    expected_node_list4 = [
+        "image",
+        "pretty boring",
+        "child",
+        "home",
+        "father",
+        "bread",
+        "wife",
+        "time",
+        "obviously",
+        "it",
+        "development",
+        "women",
+        "photo",
+    ]
+    expected_edges4 = [
+        ("image", "pretty boring"),
+        ("child", "home"),
+        ("child", "bread"),
+        ("father", "bread"),
+        ("wife", "home"),
+        ("wife", "bread"),
+        ("time", "obviously"),
+        ("it", "development"),
+        ("it", "development"),
+        ("it", "photo"),
+        ("development", "women"),
+        ("women", "time"),
+    ]
+    expected_degree4 = [
+        ("image", 1),
+        ("pretty boring", 1),
+        ("child", 2),
+        ("home", 2),
+        ("father", 1),
+        ("bread", 3),
+        ("wife", 2),
+        ("time", 2),
+        ("obviously", 1),
+        ("it", 3),
+        ("development", 3),
+        ("women", 2),
+        ("photo", 1),
+    ]
+
+    assert list(graph4.nodes()) == expected_node_list4
+    assert list(graph4.edges()) == expected_edges4
+    assert list(graph4.degree) == expected_degree4
