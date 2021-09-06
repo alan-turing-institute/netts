684ea07ef27f682072d12660d1f7d58a3b1d44ea
Oscar Giles <ogiles@turing.ac.uk>
2021-09-06 16:38:46 +0100

    ✅ Add test to make sure we can start openie server

---
+++ tests/test_openie.py
@@ -0,0 +1,58 @@
+# pylint: disable=C0114, C0116
+import logging
+import os
+import subprocess
+
+import pytest
+
+from netspy import __version__
+from netspy.config import get_settings
+
+LOGGER = logging.getLogger(__name__)
+
+
+@pytest.mark.slow
+def test_start_openie() -> None:
+
+    settings = get_settings()
+
+    # Set directory to openie_dir
+    curwd = os.getcwd()
+    os.chdir(settings.openie_dir)
+
+    # Start the server
+    process = subprocess.Popen(
+        [
+            "java",
+            "-Xmx20g",
+            "-XX:+UseConcMarkSweepGC",
+            "-jar",
+            "openie-assembly-5.0-SNAPSHOT.jar",
+            "--ignore-errors",
+            "--httpPort",
+            "6000",
+        ],
+        stdout=subprocess.PIPE,
+        universal_newlines=True,
+    )
+    while True:
+        # This is required to keep mypy happy as can be None
+        if not process.stdout:
+            raise IOError("Process can't write to standard out")
+
+        output = process.stdout.readline()
+        return_code = process.poll()
+
+        LOGGER.info("%s", output)
+        if return_code is not None:
+            raise RuntimeError("OpenIE server start up failed")
+
+        if "Server started at port 6000" in output:
+            break
+
+    assert "Server started at port 6000" in output
+
+    process.kill()
+    os.chdir(curwd)
+
+    assert os.getcwd() == curwd
