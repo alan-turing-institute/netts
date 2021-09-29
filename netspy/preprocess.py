9b8ec3f13b99dbd4daa9b0776ae4607f9ce772ab
Oscar Giles <ogiles@turing.ac.uk>
2021-09-29 16:57:08 +0100

    🐛 Fix https://github.com/alan-turing-institute/netspy/issues/71

--- netspy/preprocess.py
+++ netspy/preprocess.py
@@ -2,43 +2,36 @@
 
 import re
 from typing import Dict
+from typing import Pattern
+
 
 def replace_problematic_characters(text: str, character_map: Dict[str, str]) -> str:
     """Replace characters in a string
 
     Args:
-        text (str): String to replace characters in
-        character_map (Dict[str, str]): A dictionary where the key is the character to replace and the value is the new value
+        text: String to replace characters in
+        character_map: A dictionary where the key is the character to replace and the value is the new value
 
     Returns:
-        str: String with problematic characters replaced
+        String with problematic characters replaced
     """
 
     for symbol in character_map.keys():
         text = text.replace(symbol, character_map[symbol])
-    
+
     return text
 
-# def expand_contractions(text: str, contraction_mapping=CONTRACTION_MAP):
-#     contractions_pattern = re.compile(
-#         "({})".format("|".join(contraction_mapping.keys())),
-#         flags=re.IGNORECASE | re.DOTALL,
-#     )
 
-#     def expand_match(contraction):
-#         match = contraction.group(0)
-#         first_char = match[0]
-#         expanded_contraction = (
-#             contraction_mapping.get(match)
-#             if contraction_mapping.get(match)
-#             else contraction_mapping.get(match.lower())
-#         )
-#         expanded_contraction = first_char + expanded_contraction[1:]
-#         return expanded_contraction
+def expand_contractions(text: str, contraction_map: Dict[str, str]):
 
-#     expanded_text = contractions_pattern.sub(expand_match, text)
-#     expanded_text = re.sub("'", "", expanded_text)
-#     return expanded_text
+    map_keys = list(contraction_map.keys())
+
+    # Sort keys by length in descending order
+    map_keys.sort(key=lambda x: len(x), reverse=True)
+
+    return re.sub(
+        "({})".format("|".join(map_keys)), lambda x: contraction_map[x.group(0)], text
+    )
 
 
 # def remove_interjections(text):
