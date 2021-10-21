0476e7134c9639da8e3a912174b77d39bc2ef1b0
Iain-S <25081046+Iain-S@users.noreply.github.com>
2021-10-21 19:22:09 +0100

    Re-introduce fixture

--- tests/test_speech_graph.py
+++ tests/test_speech_graph.py
@@ -19,33 +19,33 @@ class Clients:
     corenlp_client: netspy.CoreNLPClient
 
 
-# @pytest.fixture(scope="module")
-# def module_clients() -> Generator[Any, Any, Any]:
-
-#     _ = netspy.get_settings()
-
-#     clients = Clients(
-#         openie_client=netspy.OpenIEClient(quiet=True),
-#         corenlp_client=netspy.CoreNLPClient(be_quite=True),
-#     )
-
-#     clients.openie_client.connect()
-#     clients.corenlp_client.start()
-
-#     yield clients
-
-#     clients.openie_client.close()
-#     clients.corenlp_client.stop()
-
-
-def test_speech_pickle() -> None:
+@pytest.fixture(scope="module")
+def module_clients() -> Generator[Any, Any, Any]:
 
     settings = Settings()
-    with OpenIEClient(
-        quiet=True, port=settings.netspy_config.server.openie.port
-    ) as client:
 
-        pro(openie_client=client)
+    clients = Clients(
+        openie_client=netspy.OpenIEClient(quiet=True, port=settings.netspy_config.server.openie.port),
+        corenlp_client=netspy.CoreNLPClient(be_quite=True),
+    )
+
+    clients.openie_client.connect()
+    clients.corenlp_client.start()
+
+    yield clients
+
+    clients.openie_client.close()
+    clients.corenlp_client.stop()
+
+
+def test_speech_pickle(module_clients) -> None:
+
+    # settings = Settings()
+    # with OpenIEClient(
+    #     quiet=True, port=settings.netspy_config.server.openie.port
+    # ) as client:
+
+    pro(openie_client=module_clients.openie_client)
 
     # Let the openie server shut down
     # time.sleep(5)
