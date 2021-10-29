b12962c4647fe90c8a57fcff1f52ab572f0fb844
Iain-S <25081046+Iain-S@users.noreply.github.com>
2021-10-29 09:31:56 +0100

    Remove unused function that required nltk stopwords

--- tests/test_nlp_helper_functions.py
+++ tests/test_nlp_helper_functions.py
@@ -1,6 +1,5 @@
 """Unit tests for the nlp_helper_functions module."""
 from netspy.nlp_helper_functions import (
-    process_text,
     remove_interjections,
     replace_problematic_symbols,
 )
@@ -10,13 +9,6 @@ def test_replace_problematic_symbols() -> None:
     text = "’ “ ” … ‘ – \n"
     expected = "'   ... ' -  "
     actual = replace_problematic_symbols(text)
-    assert actual == expected
-
-
-def test_process_text() -> None:
-    text = "Hmmm yes. aah no"
-    expected = "yes"
-    actual = process_text(text)
     assert actual == expected
 
 
