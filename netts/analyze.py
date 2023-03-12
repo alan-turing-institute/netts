2068cf406bbf3732a041fcd7c0d43c65a5085014
Caro Nettekoven <30801389+carobellum@users.noreply.github.com>
2023-03-11 21:01:55 -0500

    Update analyze.py

--- netts/analyze.py
+++ netts/analyze.py
@@ -1,15 +1,14 @@
 #!/Users/CN/Documents/Projects/Cambridge/cambridge_language_analysis/venv python
 # ------------------------------------------------------------------------------
-# Script name:  graph_analysis_functions.py
+# Script name:  analyze.py
 #
 # Description:
 #               Functions to analyse semantic speech graphs
 #
-# Author:       Caroline Nettekoven, 2021
+# Author:       Caroline Nettekoven, 2023
 #
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
-# source /Users/CN/Documents/Projects/Cambridge/cambridge_language_analysis/venv/bin/activate
 import networkx as nx
 import os
 import os.path as op
@@ -22,54 +21,48 @@ import numpy as np
 
 def print_bidirectional_edges(G, quiet=True):
     """Prints the bidirectional edges of a directed graph.
-        Bidirectional edges are found by:
-            # 1) Creating transpose of graph matrix
-            # 2) Checking where transpose is equal to original matrix and original matrix has non-zero value
+    Bidirectional edges are found by:
+        # 1) Creating transpose of graph matrix
+        # 2) Checking where transpose is equal to original matrix and original matrix has non-zero value
 
     Parameters
     ----------
     G : MultiDiGraph or DiGraph
         A directed graph class that can store multiedges.
     quiet : bool, optional
-        A flag indicating if output message should be muted if graph has no bidirectional edges (default is
-        True)
+        A flag indicating if output message should be muted if graph has no bidirectional edges (default is True)
 
     """
-    #
     arr = nx.to_numpy_matrix(G)
     bool_bidirectional_edges = np.logical_and(
         arr != 0, arr == np.transpose(arr)).nonzero()
     n_bidirectional_edges = bool_bidirectional_edges[0].shape[0]
     bidirectional_edges = bool_bidirectional_edges
-    if n_bidirectional_edges == 0 and quiet is True:
+    if n_bidirectional_edges == 0 and quiet:
         return n_bidirectional_edges
-    elif n_bidirectional_edges == 0 and quiet is False:
+    elif n_bidirectional_edges == 0 and not quiet:
         print(print('\n======= {} =======\nNo bidirectional edges.'.format(
             G.graph['transcript'])))
     nodes = list(G.nodes)
-    for e in range(0, n_bidirectional_edges):
-        # Ignore self loops
+    for e in range(n_bidirectional_edges):
         if bidirectional_edges[0][e] == bidirectional_edges[1][e]:
             return n_bidirectional_edges
-        else:
-            if quiet is not True:
-                print('\n======= {} ======='.format(G.graph['transcript']))
-            n1 = nodes[bidirectional_edges[0][e]]
-            n2 = nodes[bidirectional_edges[1][e]]
-            bidirectional_edge_data_1 = G.get_edge_data(n1, n2)
-            bidirectional_edge_data_2 = G.get_edge_data(n2, n1)
-            for entry in bidirectional_edge_data_1:
-                # entry = bidirectional_edge_data[0]
-                be = bidirectional_edge_data_1[entry]
-                if quiet is not True:
-                    print(
-                        '---------------------------------\n{0} \t{1} \t{2} \t{3}'.format(be['sentence'], n1, be['relation'], n2))
-            for entry in bidirectional_edge_data_2:
-                # entry = bidirectional_edge_data[0]
-                be = bidirectional_edge_data_2[entry]
-                if quiet is not True:
-                    print(
-                        '---------------------------------\n{0} \t{1} \t{2} \t{3}'.format(be['sentence'], n2, be['relation'], n1))
+        if not quiet:
+            print('\n======= {} ======='.format(G.graph['transcript']))
+        n1 = nodes[bidirectional_edges[0][e]]
+        n2 = nodes[bidirectional_edges[1][e]]
+        bidirectional_edge_data_1 = G.get_edge_data(n1, n2)
+        bidirectional_edge_data_2 = G.get_edge_data(n2, n1)
+        for entry in bidirectional_edge_data_1:
+            be = bidirectional_edge_data_1[entry]
+            if not quiet:
+                print('---------------------------------\n{0} \t{1} \t{2} \t{3}'.format(
+                    be['sentence'], n1, be['relation'], n2))
+        for entry in bidirectional_edge_data_2:
+            be = bidirectional_edge_data_2[entry]
+            if not quiet:
+                print('---------------------------------\n{0} \t{1} \t{2} \t{3}'.format(
+                    be['sentence'], n2, be['relation'], n1))
     return n_bidirectional_edges
 
 
@@ -85,32 +78,21 @@ def print_parallel_edges(G, quiet=True):
         True)
 
     """
-    #
     arr = nx.to_numpy_matrix(G)
     bool_parallel_edges = (arr >= 2)
     n_parallel_edges = np.sum(bool_parallel_edges)
-    parallel_edges = np.where(bool_parallel_edges)
-    if n_parallel_edges == 0 and quiet is True:
+    if n_parallel_edges == 0 and quiet:
         return
-    elif n_parallel_edges == 0 and quiet is not True:
-        print(print('\n======= {} =======\nNo parallel edges.'.format(
-            G.graph['transcript'])))
-    # elif n_parallel_edges == 1:
-    #     parallel_edges = np.reshape(parallel_edges, (-1, 2))
-    if quiet is not True:
-        print('\n======= {} ======='.format(G.graph['transcript']))
-    nodes = list(G.nodes)
-    # parallel_edge = parallel_edges[0]
-    for e in range(0, n_parallel_edges):
-        n1 = nodes[parallel_edges[0][e]]
-        n2 = nodes[parallel_edges[1][e]]
-        parallel_edge_data = G.get_edge_data(n1, n2)
-        for entry in parallel_edge_data:
-            # entry = parallel_edge_data[0]
-            pe = parallel_edge_data[entry]
-            if quiet is not True:
-                print(
-                    '---------------------------------\n{0} \t{1} \t{2} \t{3}'.format(pe['sentence'], n1, pe['relation'], n2))
+    transcript = G.graph['transcript']
+    if not quiet:
+        print(f"\n======= {transcript} =======")
+    for i, j in zip(*np.where(bool_parallel_edges)):
+        n1, n2 = list(G.nodes())[i], list(G.nodes())[j]
+        for entry in G.get_edge_data(n1, n2):
+            pe = G.get_edge_data(n1, n2)[entry]
+            if not quiet:
+                print('---------------------------------\n{0} \t{1} \t{2} \t{3}'.format(
+                    pe['sentence'], n1, pe['relation'], n2))
 
 
 def get_parallel_edges(G, same_sentence=True):
@@ -135,28 +117,16 @@ def get_parallel_edges(G, same_sentence=True):
             'n2'            Target node
 
     """
-    #
-    # Get parallel edges
     arr = nx.to_numpy_matrix(G)
     bool_parallel_edges = (arr >= 2)
     n_parallel_edges = np.sum(bool_parallel_edges)
-    parallel_edges = np.where(bool_parallel_edges)
     if n_parallel_edges == 0:
         return
-    # elif n_parallel_edges == 1:
-    #     parallel_edges = np.reshape(parallel_edges, (-1, 2))
-    nodes = list(G.nodes)
-    # parallel_edge = parallel_edges[0]
     G_parallel_edges = []
-    for e in range(0, n_parallel_edges):
-        n1 = nodes[parallel_edges[0][e]]
-        n2 = nodes[parallel_edges[1][e]]
-        parallel_edge_data = G.get_edge_data(n1, n2)
-        for entry in parallel_edge_data:
-            # entry = parallel_edge_data[0]
-            pe = parallel_edge_data[entry]
-            # print(
-            #     '---------------------------------\n{0} \t{1} \t{2} \t{3}'.format(pe['sentence'], n1, pe['relation'], n2))
+    for i, j in zip(*np.where(bool_parallel_edges)):
+        n1, n2 = list(G.nodes())[i], list(G.nodes())[j]
+        for entry in G.get_edge_data(n1, n2):
+            pe = G.get_edge_data(n1, n2)[entry]
             G_parallel_edges.append(
                 [G.graph['transcript'], pe['sentence'], n1, pe['relation'], n2])
     pe_df = pd.DataFrame(G_parallel_edges, columns=[
@@ -164,11 +134,6 @@ def get_parallel_edges(G, same_sentence=True):
         'sent', 'n1',
         'relation', 'n2'])
     if same_sentence:
-        if any(pe_df.duplicated(subset=['sent', 'n1', 'n2'], keep=False)):
-            # Return only those parallel edges that have been extracted from the same sentence
-            pe_df_same_sentence = pe_df[pe_df.duplicated(
-                subset=['sent', 'n1', 'n2'], keep=False)]
-            return pe_df_same_sentence
-    else:
-        # Return all parallel edges
-        return pe_df
+        pe_df = pe_df[pe_df.duplicated(
+            subset=['sent', 'n1', 'n2'], keep=False)]
+    return pe_df
